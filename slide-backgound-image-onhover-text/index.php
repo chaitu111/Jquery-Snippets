c71d6c8d1d9acdadc130e344ba85cd5efc371991
chaitu111 <koripellachaitanya11@gmail.com>
2018-09-15 19:23:42 +0530

    plugin

---
+++ slide-backgound-image-onhover-text/index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <meta charset="utf-8">
+      <!--[if IE]>
+      <meta http-equiv="x-ua-compatible" content="IE=9" />
+      <![endif]-->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>7L Express Laundry Services - Hyderabad</title>
+      <meta name="description" content="One of the best laundry services in Hyderabad">
+      <meta name="keywords" content="One of the best laundry services in Hyderabad">
+      <meta name="author" content="">
+	  <meta http-equiv="cache-control" content="no-cache" />
+	  <meta http-equiv="Pragma" content="no-cache" />
+	  <meta http-equiv="Expires" content="-1" />
+      <!-- ==============================================
+         Favicons
+         =============================================== -->
+      <link rel="icon" type="image/icon" href="<?php echo base_url() ?>assets/frontend/images/favicon.jpeg">
+      <!-- ==============================================
+         CSS VENDOR
+         =============================================== -->
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/frontend/css/vendor/bootstrap.min.css" />
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/frontend/css/vendor/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/frontend/css/vendor/simple-line-icons.css">
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/frontend/css/vendor/owl.carousel.min.css">
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/frontend/css/vendor/owl.theme.default.min.css">
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/frontend/css/vendor/magnific-popup.css">
+      <link href="https://fonts.googleapis.com/css?family=Corben" rel="stylesheet">
+      <!-- ==============================================
+         Custom Stylesheet
+         =============================================== -->
+      <link rel="stylesheet" type="text/css" href="http://kids-cloud.in/assets/frontend/css/animate.css" />
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/frontend/css/style.css" />
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/frontend/css/menu.css" />
+      <script type="text/javascript" src="<?php echo base_url() ?>assets/frontend/js/vendor/modernizr.min.js"></script>
+      <style>
+         section.call-to-action {
+         background:/* #0086d2 */#fff;
+         }
+         section.call-to-action2 {
+         background: #fff;
+         }
+         .call-to-action h1 {
+         text-transform:uppercase;
+         color:#fff;
+         font-size:30px;
+         }
+         div.owl-item{
+         width: 380px;
+         margin-right: 30px;
+         }
+         .testimony {
+         right: 60px;
+         }
+      </style>
+   </head>
+   <body>
+      <?php include('menu.php') ?>
+      <!-- BANNER -->
+	  <div class="">
+      <div id="" class="section banner ">
+         <!-- Carousel -->
+         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <!-- <ol class="carousel-indicators">
+               <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+               <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+               <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+               <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+
+               <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+
+               </ol>-->
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+               <div class="item active">
+                  <img class="mainimg hello" src="http://iniots.com/7LExpress/assets/frontend/images/background-images/bg5.png" alt="First slide">
+                  <!-- Static Header -->
+                  <div class="header-text-up">
+                     <div class="row">
+                        <div class="col-md-12 col-xs-12" style="visibility: visible;animation-duration: 700ms; animation-delay: 300ms;animation-name: fadeInRight;text-align:center;">
+                           <h2> <span>7L Express Laundry Services</span> </h2>
+                           <h3>- " We Bring The Freshness Back "</h3>
+                           <!--<div class="row">
+                              <div class="col-md-12 col-xs-12">
+                                 <a href="<?php echo base_url() ?>login" target="_blank"><img src="https://huntington.researchschool.org.uk/wp-content/uploads/sites/2/2018/04/Book-Now-RS-Blue-384x300.png" alt="First slide" class="layer-image" style="margin:0 auto;display:block"></a>
+                              </div>
+                              </div>-->
+                        </div>
+                     </div>
+                  </div>
+                  <div class="header-text">
+                     <div class="row">
+                        <div class="col-md-7 col-xs-7">
+                           <!--<img src="<?php echo base_url() ?>assets/frontend/images/layer1.png" alt="First slide" style="visibility: visible;
+                              animation-duration: 700ms;animation-delay: 300ms;animation-name: fadeInLeft;" class="img-circle" > -->
+                        </div>
+                        <div class="col-md-5 col-xs-12">
+                           <div class="section testimony">
+                              <div id="owl-testimony">
+
+                                 <div class="item">
+                                    <div class="testimonial-1">
+                                       <div class="body">
+                                          <p class="testimonials-text img2" style="">Pickup Wash and Deliver within 24hours </p>
+										<!--<p style="font-size:15px;text-align:center;"><a href="<?php echo base_url() ?>how_it_works" target="_blank">
+										  <span style="color:#ECDABA;">Know More</span></a></p>-->
+										  </div>
+                                    </div>
+                                 </div>
+                                 <div class="item">
+                                    <div class="testimonial-1">
+                                       <div class="body">
+                                          <p class="testimonials-text img3" style="">No Pre-Payments, Pay Online after Pickup</p>
+											<!--<p style="font-size:15px;text-align:center;"><a href="<?php echo base_url() ?>how_it_works" target="_blank">
+										  <span style="color:#ECDABA;">Know More</span></a></p>-->
+										  </div>
+                                    </div>
+                                 </div>
+                                 <div class="item">
+                                    <div class="testimonial-1">
+                                       <div class="body">
+                                          <p class="testimonials-text img4" style="">Hygiene Wash and Sanitation</p>
+											<!--<p style="font-size:15px;text-align:center;"><a href="<?php echo base_url() ?>how_it_works" target="_blank">
+										  <span style="color:#ECDABA;">Know More</span></a></p>-->
+										  </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <!-- Controls -->
+         </div>
+         <!-- /carousel -->
+      </div>
+
+      <!--
+         <div class="col-sm-12">
+           <p class="heading7l"> 7L Express Laundry Services </p>
+         </div>
+         <p style="text-align:center;"><b>" We Bring The Freshness Back "</b></p>
+         -->
+      <!-- TESTIMONIALS -->
+      <!--<section class="call-to-action">
+         <div class="container">
+           <div class="row">
+             <div class="col-xs-12 col-sm-12 col-lg-6 col-md-6">
+               <div class="box" style="padding-top:15px;">
+                 <div class="icon">
+                   <div class="image"><i class="fa fa-desktop"></i> </div>
+                   <div class="info">
+                     <h2 class="title"><span class="pull-left">For Website Orders</span> <span class="pull-right">Book Now</span> </h2>
+                   </div>
+                 </div>
+                 <div class="space"></div>
+               </div>
+             </div>
+             <div class="col-xs-12 col-sm-12 col-lg-6 col-md-6">
+               <div class="box" style="padding-top:15px;">
+                 <div class="icon">
+                   <div class="image"><i class="fa fa-phone"></i> </div>
+                   <div class="info">
+                     <h2 class="title"><span class="pull-left">For Tollfreee Orders</span> <span class="pull-right"> 1800-000-0000</span> </h2>
+                   </div>
+                 </div>
+                 <div class="space"></div>
+               </div>
+             </div>
+
+           </div>
+         </div>
+         </section>-->
+      <?php include('footer.php') ?>
+	    </div>
+      <!-- JS VENDOR -->
+      <script type="text/javascript" src="<?php echo base_url() ?>assets/frontend/js/vendor/jquery.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url() ?>assets/frontend/js/vendor/bootstrap.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url() ?>assets/frontend/js/vendor/owl.carousel.js"></script>
+      <script type="text/javascript" src="<?php echo base_url() ?>assets/frontend/js/vendor/jquery.superslides.js"></script>
+      <script type="text/javascript" src="<?php echo base_url() ?>assets/frontend/js/vendor/bootstrap-hover-dropdown.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url() ?>assets/frontend/js/vendor/jquery.magnific-popup.min.js"></script>
+      <!-- sendmail -->
+      <script type="text/javascript" src="<?php echo base_url() ?>assets/frontend/js/script.js"></script>
+      <script></script>
+      <script>var hotbod = document.querySelector("body");
+         function doStuff() {
+             hotbod.className += " animate";
+         }
+
+         window.onload = function() {
+             doStuff();
+         };
+      </script>
+      <script type="text/javascript">
+         /*global $ */
+         $(document).ready(function () {
+
+            "use strict";
+
+            $('.menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
+            //Checks if li has sub (ul) and adds class for toggle icon - just an UI
+
+
+            $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
+            //Checks if drodown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu (thanks Luka Kladaric)
+
+            $(".menu > ul").before("<a href=\"#\" class=\"menu-mobile\">Menu</a>");
+
+            //Adds menu-mobile class (for mobile toggle menu) before the normal menu
+            //Mobile menu is hidden if width is more then 959px, but normal menu is displayed
+            //Normal menu is hidden if width is below 959px, and jquery adds mobile menu
+            //Done this way so it can be used with wordpress without any trouble
+
+            $(".menu > ul > li").hover(function (e) {
+                if ($(window).width() > 943) {
+                    $(this).children("ul").stop(true, false).fadeToggle(150);
+                    e.preventDefault();
+                }
+            });
+            //If width is more than 943px dropdowns are displayed on hover
+
+            $(".menu > ul > li").click(function () {
+                if ($(window).width() <= 943) {
+                    $(this).children("ul").fadeToggle(150);
+                }
+            });
+            //If width is less or equal to 943px dropdowns are displayed on click (thanks Aman Jain from stackoverflow)
+
+            $(".menu-mobile").click(function (e) {
+                $(".menu > ul").toggleClass('show-on-mobile');
+                e.preventDefault();
+            });
+            //when clicked on mobile-menu, normal menu is shown as a list, classic rwd menu story (thanks mwl from stackoverflow)
+
+         });
+      </script>
+      <script>
+         $(document).ready(function(){
+
+         	  $(".img1").on({
+                 mouseenter: function(){
+                      $('.mainimg').attr({'src' : 'http://iniots.com/7LExpress/assets/frontend/images/background-images/bg1.jpg','background-repeat': 'no-repeat'});
+                 },
+                 mouseleave: function()
+         		{
+         			$('.mainimg').attr({'src' : 'http://iniots.com/7LExpress/assets/frontend/images/background-images/bg5.png','background-repeat': 'no-repeat'});
+                 },
+             });
+
+         	 $(".img2").on({
+                 mouseenter: function(){
+                      $('.mainimg').attr({'src' : 'http://iniots.com/7LExpress/assets/frontend/images/background-images/bg2.jpg','background-repeat': 'no-repeat'});
+                 },
+                 mouseleave: function()
+         		{
+         			$('.mainimg').attr({'src' : 'http://iniots.com/7LExpress/assets/frontend/images/background-images/bg5.png','background-repeat': 'no-repeat'});
+                 },
+             });
+
+         	 $(".img3").on({
+                 mouseenter: function(){
+                      $('.mainimg').attr({'src' : 'http://iniots.com/7LExpress/assets/frontend/images/background-images/bg3.jpg','background-repeat': 'no-repeat'});
+                 },
+                 mouseleave: function()
+         		{
+         			$('.mainimg').attr({'src' : 'http://iniots.com/7LExpress/assets/frontend/images/background-images/bg5.png','background-repeat': 'no-repeat'});
+                 },
+             });
+
+         	 $(".img4").on({
+                 mouseenter: function(){
+                      $('.mainimg').attr({'src' : 'http://iniots.com/7LExpress/assets/frontend/images/background-images/bg4.jpg','background-repeat': 'no-repeat'});
+                 },
+                 mouseleave: function()
+         		{
+         			$('.mainimg').attr({'src' : 'http://iniots.com/7LExpress/assets/frontend/images/background-images/bg5.png','background-repeat': 'no-repeat'});
+                 },
+             });
+
+         });
+      </script>
+   </body>
+</html>
